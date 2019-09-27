2afaf3f1db45ee80ea5f221f1f90c9fa65c86e2a
Cheng <1334083811@qq.com>
2019-09-27 17:12:22 +0800

    add logout

--- src/main/java/com/cheng/community/controller/AuthorizeController.java
+++ src/main/java/com/cheng/community/controller/AuthorizeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
@@ -71,5 +72,17 @@ public class AuthorizeController {
             //登录失败，重新登陆
             return "redirect:/";
         }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request,
+                         HttpServletResponse response){
+        // 移除session
+        request.getSession().removeAttribute("user");
+        // 删除cookie
+        Cookie cookie = new Cookie("token", null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "redirect:/";
     }
 }
